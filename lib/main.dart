81724a0d7ec2b419547cb4891e4d6a20ab1fd1db
yugal-panchal <yugalpanchal01@gmail.com>
2023-05-14 00:37:20 +0530

    Firebase integration done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:seek_assesment/views/dashboard_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
